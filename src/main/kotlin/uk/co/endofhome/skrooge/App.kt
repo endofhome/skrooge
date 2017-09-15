88138c7ee979d5a7c7fed415843cd8a0eb99a456
Tom Barnes <tom.barnes@springer.com>
2017-09-15 11:33:46 +0100

    basic setup for http4k app

---
+++ src/main/kotlin/uk/co/endofhome/skrooge/App.kt
@@ -0,0 +1,22 @@
+package uk.co.endofhome.skrooge
+
+import org.http4k.core.Method.GET
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+import org.http4k.server.Jetty
+import org.http4k.server.asServer
+
+fun main(args: Array<String>) {
+    val port = if (args.isNotEmpty()) args[0].toInt() else 5000
+
+    val app = App().routes()
+
+    app.asServer(Jetty(port)).startAndBlock()
+}
+
+class App {
+    fun routes() = routes("/" bind GET to { _: Request -> Response(OK).body("Hello World!") })
+}
