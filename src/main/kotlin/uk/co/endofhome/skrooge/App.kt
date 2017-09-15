c95f4f9035edab45e0dda4977605943d97bc0f43
Tom Barnes <tom.barnes@springer.com>
2017-09-15 15:21:48 +0100

    parse good form data and read file on FS, return bad request for bad form data

--- src/main/kotlin/uk/co/endofhome/skrooge/App.kt
+++ src/main/kotlin/uk/co/endofhome/skrooge/App.kt
@@ -4,12 +4,16 @@ import org.http4k.asString
 import org.http4k.core.Body
 import org.http4k.core.Method.POST
 import org.http4k.core.Response
+import org.http4k.core.Status.Companion.BAD_REQUEST
 import org.http4k.core.Status.Companion.NOT_FOUND
 import org.http4k.core.Status.Companion.OK
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
+import java.io.File
+import java.time.Month
+import java.time.Year
 
 fun main(args: Array<String>) {
     val port = if (args.isNotEmpty()) args[0].toInt() else 5000
@@ -22,11 +26,35 @@ class App {
 }
 
 class Statements {
+    private val parser = PretendFormParser()
+
     fun uploadStatements(body: Body): Response {
       return if (body.payload.asString().isEmpty()) {
           Response(NOT_FOUND)
       } else {
+          try {
+              val statementData: StatementData = parser.parse(body)
+              statementData.files.forEach {
+                  val fileData = it.readLines()
+                  println(fileData)
+              }
+          } catch (e: Exception) {
+              return Response(BAD_REQUEST)
+          }
           Response(OK)
       }
     }
 }
+
+class PretendFormParser {
+    fun parse(body: Body): StatementData {
+        val params = body.payload.asString().split(",")
+        val year = Year.parse(params[0])
+        val month = Month.valueOf(params[1].toUpperCase())
+        val username = params[2]
+        val files: List<File> = listOf(params[3]).map { File(it) }
+        return StatementData(year, month, username, files)
+    }
+}
+
+data class StatementData(val year: Year, val month: Month, val username: String, val files: List<File>)
--- src/test/kotlin/uk/co/endofhome/skrooge/SkroogeTest.kt
+++ src/test/kotlin/uk/co/endofhome/skrooge/SkroogeTest.kt
@@ -6,24 +6,31 @@ import org.http4k.core.Body
 import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
+import org.http4k.core.Status.Companion.BAD_REQUEST
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.Status.Companion.NOT_FOUND
 import org.junit.Test
-import java.io.InputStream
 
 class SkroogeTest {
     val skrooge = App().routes()
 
     @Test
-    fun `POST to statements endpoint with empty body returns 404`() {
+    fun `POST to statements endpoint with empty body returns HTTP Not Found`() {
         val request = Request(POST, "/statements").body(Body.EMPTY)
         assertThat(skrooge(request), equalTo(Response(NOT_FOUND)))
     }
 
     @Test
-    fun `POST to statements with a dummy string instead of form data returns 200`() {
+    fun `POST to statements with a dummy string instead of form data returns HTTP OK`() {
         // Can't receive form data, so will read files from FS instead. Will use a string to represent form.
-        val request = Request(POST, "/statements").body("year=2017, month=September, user=Tom, files=[empty-file.csv]")
+        val request = Request(POST, "/statements").body("2017,September,Tom,src/test/resources/empty-file.csv")
         assertThat(skrooge(request), equalTo(Response(OK)))
+    }
+
+    @Test
+    fun `POST to statements with incorrect dummy form data returns HTTP Bad Request`() {
+        // Can't receive form data, so will read files from FS instead. Will use a string to represent form.
+        val request = Request(POST, "/statements").body("nonsense")
+        assertThat(skrooge(request), equalTo(Response(BAD_REQUEST)))
     }
 }
