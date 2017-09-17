474d76b37722c2e4e6ac43844c2103eef6f0b11e
Tom Barnes <ruralmyth@gmail.com>
2017-09-17 10:04:42 +0100

    rewritten tests with http4k hamkrest matchers

--- src/test/kotlin/uk/co/endofhome/skrooge/SkroogeTest.kt
+++ src/test/kotlin/uk/co/endofhome/skrooge/SkroogeTest.kt
@@ -3,7 +3,7 @@ package uk.co.endofhome.skrooge
 import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.containsSubstring
 import com.natpryce.hamkrest.assertion.assertThat
-import org.http4k.asString
+import com.natpryce.hamkrest.should.shouldMatch
 import org.http4k.core.*
 import org.http4k.core.Method.POST
 import org.http4k.core.Status.Companion.BAD_REQUEST
@@ -11,6 +11,7 @@ import org.http4k.core.Status.Companion.OK
 import org.http4k.core.Status.Companion.NOT_FOUND
 import org.http4k.core.Status.Companion.SEE_OTHER
 import org.http4k.format.Jackson
+import org.http4k.hamkrest.*
 import org.junit.Before
 import org.junit.Test
 import java.io.File
@@ -26,20 +27,20 @@ class SkroogeTest {
     @Test
     fun `POST to statements endpoint with empty body returns HTTP Not Found`() {
         val request = Request(POST, "/statements").body(Body.EMPTY)
-        assertThat(skrooge(request), equalTo(Response(NOT_FOUND)))
+        skrooge(request) shouldMatch hasStatus(NOT_FOUND)
     }
 
     @Test
     fun `POST to statements with incorrect dummy form data returns HTTP Bad Request`() {
         // Can't receive form data, so will read files from FS instead. Will use a string to represent form.
         val request = Request(POST, "/statements").body("nonsense")
-        assertThat(skrooge(request), equalTo(Response(BAD_REQUEST)))
+        skrooge(request) shouldMatch hasStatus(BAD_REQUEST)
     }
 
     @Test
     fun `POST to statements with a dummy string instead of form data returns HTTP OK`() {
         val request = Request(POST, "/statements").body("2017,September,Tom,src/test/resources/empty-file.csv")
-        assertThat(skrooge(request), equalTo(Response(OK)))
+        skrooge(request) shouldMatch hasStatus(OK)
     }
 
     @Test
@@ -68,10 +69,12 @@ class SkroogeTest {
         val requestWithMcDonalds = Request(POST, "/statements").body("2017,September,Tom,src/test/resources/unknown-transaction.csv")
         val response = skrooge(requestWithMcDonalds)
         val unrecognisedTransactions = Jackson.asJsonObject(UnknownTransaction(listOf(ProcessedLine(true, "McDonalds", ""))))
-        assertThat(response, equalTo(Response(SEE_OTHER).header("Location", "/unknown-transaction").body(unrecognisedTransactions.toString())))
+        response shouldMatch hasStatus(SEE_OTHER)
+        response shouldMatch hasHeader("Location", "/unknown-transaction")
+        response shouldMatch hasBody(unrecognisedTransactions.toString())
 
         val followedResponse = followRedirectResponse(response)
-        assertThat(followedResponse.body.payload.asString(), containsSubstring("You need to categorise some transactions."))
+        followedResponse shouldMatch hasBody(containsSubstring("You need to categorise some transactions."))
     }
 
     @Test
@@ -79,7 +82,7 @@ class SkroogeTest {
         val requestWithMcDonalds = Request(POST, "/statements").body("2017,September,Tom,src/test/resources/unknown-transaction.csv")
         val followedResponse = followRedirectResponse(skrooge(requestWithMcDonalds))
 
-        assertThat(followedResponse.body.payload.asString(), containsSubstring("McDonalds"))
+        followedResponse shouldMatch hasBody(containsSubstring("McDonalds"))
     }
 
     private fun followRedirectResponse(response: Response): Response {
