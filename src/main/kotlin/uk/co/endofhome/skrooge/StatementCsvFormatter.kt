5e6cf1205e1bf08f8206c2460d657fc7748e5252
Tom Barnes <ruralmyth@gmail.com>
2017-11-13 18:23:48 +0000

    add first CSV formatter - tests and test data are ignored for privacy reasons

---
+++ src/main/kotlin/uk/co/endofhome/skrooge/StatementCsvFormatter.kt
@@ -0,0 +1,72 @@
+package uk.co.endofhome.skrooge
+
+import java.io.File
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.time.Month
+
+object StatementCsvFormatter {
+    val baseInputPath = Paths.get("input")
+
+    operator fun invoke(inputFilePath: Path): List<String> {
+        val file = File(baseInputPath.toString() + File.separator + inputFilePath.toString())
+
+        return file.readLines()
+                .filterNot { it.contains("Payment") }
+                .map {
+            val sanitisedLine = removeTrailingCommasFrom(it)
+            val split = sanitisedLine.split(",")
+            val date = dateFrom(split[0])
+            val sanitisedMerchant = sanitize(split[1])
+            val value = split.last()
+
+            "$date,$sanitisedMerchant,$value"
+        }
+    }
+
+    private fun removeTrailingCommasFrom(line: String): String {
+        return line.let { when {
+                it.endsWith(",") -> line.removeSuffix(",")
+                else -> line
+            }
+        }
+    }
+
+    private fun sanitize(merchant: String): String {
+        val prefixRemoved = merchant.replace("\" ", "")
+        return when {
+            prefixRemoved.startsWith("Tesco") -> "Tesco"
+            prefixRemoved.startsWith("B & Q") -> "B&Q"
+            prefixRemoved.startsWith("Spotify") -> "Spotify"
+            prefixRemoved.startsWith("Wickes") -> "Wickes"
+            else -> prefixRemoved
+        }
+    }
+
+    private fun dateFrom(barclaycardDate: String): String {
+        val dateElements = barclaycardDate.split(" ")
+        val day = dateElements[0]
+        val month = monthFrom(dateElements[1])?.value
+        val year = "20" + dateElements[2]
+        return "$year-$month-$day"
+    }
+
+    private fun monthFrom(shortName: String): Month? {
+        val monthMap = mapOf(
+                "Jan" to Month.JANUARY,
+                "Feb" to Month.FEBRUARY,
+                "Mar" to Month.MARCH,
+                "Apr" to Month.APRIL,
+                "May" to Month.MAY,
+                "Jun" to Month.JUNE,
+                "Jul" to Month.JULY,
+                "Aug" to Month.AUGUST,
+                "Sep" to Month.SEPTEMBER,
+                "Oct" to Month.OCTOBER,
+                "Nov" to Month.NOVEMBER,
+                "Dec" to Month.DECEMBER
+        )
+
+        return monthMap[shortName]
+    }
+}
