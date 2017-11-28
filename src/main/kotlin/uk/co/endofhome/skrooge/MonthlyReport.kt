16e834c5813ad795c6bfdc56b853b514e11a381e
Tom Barnes <ruralmyth@gmail.com>
2017-11-28 22:51:11 +0000

    round actual values in JSON reports

--- src/main/kotlin/uk/co/endofhome/skrooge/MonthlyReport.kt
+++ src/main/kotlin/uk/co/endofhome/skrooge/MonthlyReport.kt
@@ -1,12 +1,12 @@
 package uk.co.endofhome.skrooge
 
-import org.http4k.core.Request
-import org.http4k.core.Response
-import org.http4k.core.Status
+import org.http4k.core.*
 import org.http4k.format.Gson
+import java.math.BigDecimal
+import java.math.RoundingMode
 import java.time.Month
 import java.time.format.TextStyle
-import java.util.Locale
+import java.util.*
 
 class MonthlyReport(val gson: Gson, val decisionWriter: DecisionWriter) {
     fun handle(request: Request): Response {
@@ -20,7 +20,7 @@ class MonthlyReport(val gson: Gson, val decisionWriter: DecisionWriter) {
                         CategoryReportDataItem(it.subCategory!!.name, it.line.amount)
                     }.groupBy { it.name }.map {
                         it.value.reduce { acc, categoryReportDataItem -> CategoryReportDataItem(it.key, acc.actual + categoryReportDataItem.actual) }
-                    }
+                    }.map { it.copy(actual = BigDecimal.valueOf(it.actual).setScale(2, RoundingMode.HALF_UP).toDouble()) }
 
                     val catReports = CategoryHelpers.categories().map { category ->
                         CategoryReport(category.title, catReportDataItems.filter { category.subcategories.map { it.name }.contains(it.name) })
