b5b66477eb10f04e0e889cd7206c6dc1dc9a097c
Tom Barnes <ruralmyth@gmail.com>
2018-08-30 16:55:05 +0100

    Don't use wildcard imports

--- src/main/kotlin/uk/co/endofhome/skrooge/scripts/StatementProcessorScript.kt
+++ src/main/kotlin/uk/co/endofhome/skrooge/scripts/StatementProcessorScript.kt
@@ -1,6 +1,10 @@
 package uk.co.endofhome.skrooge.scripts
 
-import uk.co.endofhome.skrooge.*
+import uk.co.endofhome.skrooge.csvformatters.BankFiveStatementCsvFormatter
+import uk.co.endofhome.skrooge.csvformatters.BankOneStatementCsvFormatter
+import uk.co.endofhome.skrooge.csvformatters.BankSixStatementCsvFormatter
+import uk.co.endofhome.skrooge.csvformatters.BankThreeStatementCsvFormatter
+import uk.co.endofhome.skrooge.csvformatters.BankTwoStatementCsvFormatter
 import java.nio.file.Paths
 
 fun main(args: Array<String>) {
