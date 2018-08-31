ba177e525450fc8172417783ad8e615754869a8d
Tom Barnes <ruralmyth@gmail.com>
2018-08-31 12:36:35 +0100

    Don't really on these defaults either

--- src/test/kotlin/uk/co/endofhome/skrooge/BankReportsTest.kt
+++ src/test/kotlin/uk/co/endofhome/skrooge/BankReportsTest.kt
@@ -2,19 +2,26 @@ package uk.co.endofhome.skrooge
 
 import com.natpryce.hamkrest.containsSubstring
 import com.natpryce.hamkrest.should.shouldMatch
-import org.http4k.core.*
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Status
 import org.http4k.hamkrest.hasBody
 import org.http4k.hamkrest.hasStatus
 import org.junit.Test
 
 class BankReportsTest {
 
-    val skrooge = Skrooge().routes()
+    private val categories = CategoryHelpers.categories("src/test/resources/test-schema.json")
+    private val categoryMappings = mutableListOf("Pizza Union,Some category,Some subcategory")
+    private val skrooge = Skrooge(categories, categoryMappings).routes()
 
     @Test
     fun `Statement categorisation report has required hidden fields`() {
         val request = Request(Method.POST, "/statements").body("2016;January;Test;[input/normalised/2016-01_Test_Santander.csv]")
-        skrooge(request) shouldMatch hasStatus(Status.OK)
-        skrooge(request) shouldMatch hasBody(containsSubstring("<input type=\"hidden\" name=\"statement-data\" value=\"2016;January;Test;[Santander.csv]\">"))
+
+        val response = skrooge(request)
+
+        response shouldMatch hasStatus(Status.OK)
+        response shouldMatch hasBody(containsSubstring("<input type=\"hidden\" name=\"statement-data\" value=\"2016;January;Test;[Santander.csv]\">"))
     }
 }
