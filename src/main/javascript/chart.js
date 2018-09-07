8e06bd06b515fbe704b4312912e276d1057ca6ff
Tom Barnes <ruralmyth@gmail.com>
2018-09-07 10:10:55 +0100

    Show all categories on monthly chart view. Don't blow up or show charts that don't have any data.

--- src/main/javascript/chart.js
+++ src/main/javascript/chart.js
@@ -13,10 +13,8 @@ const _dataForCategory = (title, categories) => {
     if (matchingCategories.length === 1) {
         return matchingCategories[0].data
     } else if (matchingCategories.length === 0) {
-        throw {
-            name: 'No matching categories',
-            message: 'No matching categories for title:' + title
-        }
+        console.log('No matching categories for title:' + title);
+        return []
     } else if (matchingCategories.length > 1) {
         throw {
             name: 'Too many matching categories',
@@ -28,42 +26,57 @@ const _dataForCategory = (title, categories) => {
 function generateCategory(title, categories, binding, height) {
     const dataForCategory = _dataForCategory(title, categories);
 
-    const c3Data = {
-        bindto: binding,
-        data: {
-            labels: true,
-            json: dataForCategory,
-            keys: {
-                x: 'name',
-                value: ['actual', 'budget']
+    if (dataForCategory.length > 0) {
+        const c3Data = {
+            bindto: binding,
+            title: {
+                text: title
             },
-            type: 'bar',
-            color: (color, d) => {
-                return colourtron(d, dataForCategory)
+            data: {
+                labels: true,
+                json: dataForCategory,
+                keys: {
+                    x: 'name',
+                    value: ['actual', 'budget']
+                },
+                type: 'bar',
+                color: (color, d) => {
+                    return colourtron(d, dataForCategory)
+                }
+            },
+            bar: {
+                width: {
+                    ratio: 0.5
+                }
+            },
+            axis: {
+                rotated: true,
+                x: {
+                    type: 'category'
+                }
             }
-        },
-        bar: {
-            width: {
-                ratio: 0.5
-            }
-        },
-        axis: {
-            rotated: true,
-            x: {
-                type: 'category'
-            }
-        }
-    };
+        };
 
-    if (height !== null) {
-        c3Data.size = {
-            height: height
+        if (height !== null) {
+            c3Data.size = {
+                height: height
+            }
         }
+
+        c3.generate(c3Data);
     }
-
-    c3.generate(c3Data);
 }
 
 monthlyReportData().then((result => {
     generateCategory("In your home", result.categories, "#in-your-home", 1000);
+    generateCategory("Insurance", result.categories, "#insurance");
+    generateCategory("Motoring and public transport", result.categories, "#motoring-and-public-transport");
+    generateCategory("Savings and investments", result.categories, "#savings-and-investments");
+    generateCategory("Family", result.categories, "#family");
+    generateCategory("Fun", result.categories, "#fun");
+    generateCategory("Health and beauty", result.categories, "#health-and-beauty");
+    generateCategory("Clothes", result.categories, "#clothes");
+    generateCategory("Big one offs", result.categories, "#big-one-offs");
+    generateCategory("Odds and sods", result.categories, "#odds-and-sods");
+    generateCategory("Refurbishments", result.categories, "#refurbishments");
 }));
