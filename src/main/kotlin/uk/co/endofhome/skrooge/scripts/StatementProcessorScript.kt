0720d4e3a5e9ef5a67722f7d5cbb0ef96e7a9527
Tom Barnes <ruralmyth@gmail.com>
2018-09-09 19:17:06 +0100

    StatementProcessorScript can now also write out approved files with correct format filename

--- src/main/kotlin/uk/co/endofhome/skrooge/scripts/StatementProcessorScript.kt
+++ src/main/kotlin/uk/co/endofhome/skrooge/scripts/StatementProcessorScript.kt
@@ -1,27 +1,45 @@
 package uk.co.endofhome.skrooge.scripts
 
-import uk.co.endofhome.skrooge.csvformatters.BankFiveStatementCsvFormatter
 import uk.co.endofhome.skrooge.csvformatters.BankOneStatementCsvFormatter
-import uk.co.endofhome.skrooge.csvformatters.BankSixStatementCsvFormatter
-import uk.co.endofhome.skrooge.csvformatters.BankThreeStatementCsvFormatter
-import uk.co.endofhome.skrooge.csvformatters.BankTwoStatementCsvFormatter
+import java.io.File
 import java.nio.file.Paths
+import java.time.Month
 
 fun main(args: Array<String>) {
 
-    val bank = ""
-    val statementFileName = ""
+    val bankStatementFormatter = BankOneStatementCsvFormatter
+    val statementFileName = "bank_accountType_monthName_year.csv"
 
-    val formattedStatement = formatterMap()[bank]!!.invoke(Paths.get(statementFileName))
+    val formattedStatement = bankStatementFormatter.invoke(Paths.get(statementFileName))
     formattedStatement.forEach {
         println(it)
     }
+
+//    approve(formattedStatement, statementFileName, "Bill")
 }
 
-fun formatterMap() = mapOf(
-        "BANK_ONE" to BankOneStatementCsvFormatter,
-        "BANK_TWO" to BankTwoStatementCsvFormatter,
-        "BANK_THREE" to BankThreeStatementCsvFormatter,
-        "BANK_FIVE" to BankFiveStatementCsvFormatter,
-        "BANK_SIX" to BankSixStatementCsvFormatter
-)
+fun normalisedFilenameFor(statementFilename: String, username: String): String {
+    val filenameParts = statementFilename.substringBefore('.').split('_')
+    val bank = filenameParts[0]
+    val accountType = filenameParts[1]
+    val monthName = filenameParts[2]
+    val year = filenameParts[3]
+
+    val month = Month.valueOf(monthName.toUpperCase()).value.toString().padStart(2, '0')
+    val user = username.capitalize()
+    val bankdetails = bank.capitalize() + accountType.capitalize()
+
+    val fileExtension = "csv"
+    return "$year-${month}_${user}_$bankdetails.$fileExtension"
+}
+
+fun approve(normalisedStatement: List<String>, statementFilename: String, username: String) {
+    val normalisedFilename = normalisedFilenameFor(statementFilename, username)
+
+    val printWriter = File("input/normalised/$normalisedFilename").printWriter()
+    normalisedStatement.forEach {
+        printWriter.write(it + System.getProperty("line.separator"))
+    }
+
+    printWriter.close()
+}
