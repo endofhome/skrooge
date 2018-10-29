3081181d47e80b6d4d8ee4d9d92ffb2d6697b5ef
Tom Barnes <ruralmyth@gmail.com>
2018-10-29 19:02:54 +0000

    Use destructuring for clarity. Reformat

--- src/main/kotlin/uk/co/endofhome/skrooge/statements/StatementDecider.kt
+++ src/main/kotlin/uk/co/endofhome/skrooge/statements/StatementDecider.kt
@@ -8,25 +8,25 @@ import java.time.LocalDate
 
 class StatementDecider(categoryMappings: List<String>) {
     private val mappings = categoryMappings.map {
-        val mappingStrings = it.split(",")
-        CategoryMapping(mappingStrings[0], mappingStrings[1], mappingStrings[2])
+        val (purchase, mainCategory, subCategory) = it.split(",")
+        CategoryMapping(purchase, mainCategory, subCategory)
     }
 
     fun process(statementData: List<String>) = statementData.map { decide(it) }
 
     private fun decide(lineString: String): Decision {
-        val lineEntries = lineString.split(",")
-        val dateValues = lineEntries[0].split("-").map { it.toInt() }
-        val line = Line(LocalDate.of(dateValues[0], dateValues[1], dateValues[2]), lineEntries[1], lineEntries[2].toDouble())
+        val (date, merchant, amount) = lineString.split(",")
+        val (year, month, day) = date.split("-").map { it.toInt() }
+        val line = Line(
+            LocalDate.of(year, month, day),
+            merchant,
+            amount.toDouble()
+        )
 
         val match = mappings.find { it.purchase.contains(line.merchant) }
         return when (match) {
-            null -> {
-                Decision(line, null, null)
-            }
-            else -> {
-                Decision(line, Category(match.mainCategory, emptyList()), SubCategory(match.subCategory))
-            }
+            null -> Decision(line, null, null)
+            else -> Decision(line, Category(match.mainCategory, emptyList()), SubCategory(match.subCategory))
         }
     }
 }
