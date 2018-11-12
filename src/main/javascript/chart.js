63c6ce20e4113a96b9c938991b8110e17c73e967
Tom Barnes <ruralmyth@gmail.com>
2018-11-12 07:57:49 +0000

    Show message on page if data isn't available for a specific category, or if there is no data available at all for a given month.

--- src/main/javascript/chart.js
+++ src/main/javascript/chart.js
@@ -15,68 +15,83 @@ const xAxisValues = {
 };
 
 function generateCategory(title, categoryData, binding, xAxisValue, height) {
-    if (categoryData.length > 0) {
-        const c3Data = {
-            bindto: binding,
-            title: {
-                text: title
+    const c3Data = {
+        bindto: binding,
+        title: {
+            text: title
+        },
+        data: {
+            labels: true,
+            json: categoryData,
+            keys: {
+                x: 'name',
+                value: xAxisValue || xAxisValues.generic
             },
-            data: {
-                labels: true,
-                json: categoryData,
-                keys: {
-                    x: 'name',
-                    value: xAxisValue || xAxisValues.generic
-                },
-                type: 'bar',
-                color: (color, d) => {
-                    return colourtron(d, categoryData)
+            empty: {
+                label: {
+                    text: "No Data"
                 }
             },
-            bar: {
-                width: {
-                    ratio: 0.5
-                }
-            },
-            axis: {
-                rotated: true,
-                x: {
-                    type: 'category'
-                }
+            type: 'bar',
+            color: (color, d) => {
+                return colourtron(d, categoryData)
             }
-        };
-
-        if (height !== null) {
-            c3Data.size = {
-                height: height
+        },
+        bar: {
+            width: {
+                ratio: 0.5
+            }
+        },
+        axis: {
+            rotated: true,
+            x: {
+                type: 'category'
             }
         }
+    };
 
-        c3.generate(c3Data);
+    if (height !== null) {
+        c3Data.size = {
+            height: height
+        }
     }
+    c3.generate(c3Data);
 }
 
 const reformatForAnnualOverview = (genericFormatData) => {
-    return [{ name: genericFormatData.name, actual: genericFormatData.yearToDateActual, budget: genericFormatData.yearToDateBudget, annualBudget: genericFormatData.annualBudget }]
+    return [{
+        name: genericFormatData.name,
+        actual: genericFormatData.yearToDateActual,
+        budget: genericFormatData.yearToDateBudget,
+        annualBudget: genericFormatData.annualBudget
+    }]
 };
 
 const reformatForMonthOverview = (genericFormatData) => {
-    return [{ name: genericFormatData.name, actual: genericFormatData.actual, budget: genericFormatData.budget }]
+    return [{
+        name: genericFormatData.name,
+        actual: genericFormatData.actual,
+        budget: genericFormatData.budget
+    }]
 };
 
 monthlyReportData().then((result => {
-    generateCategory("Annual overview", reformatForAnnualOverview(result.aggregateOverview.data), "#annual-overview", xAxisValues.annualOverview);
-    generateCategory("Month overview", reformatForMonthOverview(result.aggregateOverview.data), "#month-overview");
-    generateCategory("Month breakdown", result.overview.data, "#month-breakdown", xAxisValues.generic, 1000);
-    generateCategory("In your home", dataForCategory("In your home", result.categories), "#in-your-home", xAxisValues.generic, 1000);
-    generateCategory("Insurance", dataForCategory("Insurance", result.categories), "#insurance");
-    generateCategory("Motoring and public transport", dataForCategory("Motoring and public transport", result.categories), "#motoring-and-public-transport");
-    generateCategory("Savings and investments", dataForCategory("Savings and investments", result.categories), "#savings-and-investments");
-    generateCategory("Family", dataForCategory("Family", result.categories), "#family");
-    generateCategory("Fun", dataForCategory("Fun", result.categories), "#fun");
-    generateCategory("Health and beauty", dataForCategory("Health and beauty", result.categories), "#health-and-beauty");
-    generateCategory("Clothes", dataForCategory("Clothes", result.categories), "#clothes");
-    generateCategory("Big one offs", dataForCategory("Big one offs", result.categories), "#big-one-offs");
-    generateCategory("Odds and sods", dataForCategory("Odds and sods", result.categories), "#odds-and-sods");
-    generateCategory("Refurbishments", dataForCategory("Refurbishments", result.categories), "#refurbishments");
+    if (Object.keys(result).length) {
+        generateCategory("Annual overview", reformatForAnnualOverview(result.aggregateOverview.data), "#annual-overview", xAxisValues.annualOverview);
+        generateCategory("Month overview", reformatForMonthOverview(result.aggregateOverview.data), "#month-overview");
+        generateCategory("Month breakdown", result.overview.data, "#month-breakdown", xAxisValues.generic, 1000);
+        generateCategory("In your home", dataForCategory("In your home", result.categories), "#in-your-home", xAxisValues.generic, 1000);
+        generateCategory("Insurance", dataForCategory("Insurance", result.categories), "#insurance");
+        generateCategory("Motoring and public transport", dataForCategory("Motoring and public transport", result.categories), "#motoring-and-public-transport");
+        generateCategory("Savings and investments", dataForCategory("Savings and investments", result.categories), "#savings-and-investments");
+        generateCategory("Family", dataForCategory("Family", result.categories), "#family");
+        generateCategory("Fun", dataForCategory("Fun", result.categories), "#fun");
+        generateCategory("Health and beauty", dataForCategory("Health and beauty", result.categories), "#health-and-beauty");
+        generateCategory("Clothes", dataForCategory("Clothes", result.categories), "#clothes");
+        generateCategory("Big one offs", dataForCategory("Big one offs", result.categories), "#big-one-offs");
+        generateCategory("Odds and sods", dataForCategory("Odds and sods", result.categories), "#odds-and-sods");
+        generateCategory("Refurbishments", dataForCategory("Refurbishments", result.categories), "#refurbishments");
+    } else {
+        document.getElementById("month-unavailable").innerText = "Sorry, this month is not available yet.";
+    }
 }));
