7614152ad0cf1cc60077361282f49a8c40f417ad
Tom Barnes <ruralmyth@gmail.com>
2018-11-16 07:59:13 +0000

    Restructuring / reformatting for clarity

--- src/main/kotlin/uk/co/endofhome/skrooge/csvformatters/StatementCsvFormatter.kt
+++ src/main/kotlin/uk/co/endofhome/skrooge/csvformatters/StatementCsvFormatter.kt
@@ -14,20 +14,21 @@ interface StatementCsvFormatter {
         var specialCharacterDetected = false
         val numericCharacters = listOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
         val specialCharacters = listOf('\'', '&', '/', ' ', '*') + numericCharacters
-        return this.map {
-            when (specialCharacters.contains(it)) {
-                true -> {
-                    specialCharacterDetected = true
-                    it
-                }
-                false -> {
-                    if (specialCharacterDetected == true) {
-                        specialCharacterDetected = false
-                        it.toUpperCase()
-                    } else it
+        return this.map { char ->
+            if (specialCharacters.contains(char)) {
+                specialCharacterDetected = true
+                char
+            }
+            else {
+                if (specialCharacterDetected) {
+                    specialCharacterDetected = false
+                    char.toUpperCase()
+                } else {
+                    char
                 }
             }
-        }.joinToString("").capitalizeUnlessWebAddress()
+        }.joinToString("")
+         .capitalizeUnlessWebAddress()
     }
 
     fun String.capitalizeUnlessWebAddress(): String =
