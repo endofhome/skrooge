bb27c4d7f08057597525bc52182e90c8d640ebe4
Tom Barnes <ruralmyth@gmail.com>
2018-11-19 17:48:25 +0000

    set Webpack mode to 'none'

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: "./src/main/javascript/chart",
+    mode: "none",
     output: {
         path: path.resolve(__dirname, "public"),
         filename: "bundle.js",
