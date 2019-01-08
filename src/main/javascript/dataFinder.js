0c1672d974124a582f71827acab8500b1f9053bc
Tom Barnes <ruralmyth@gmail.com>
2019-01-08 22:13:53 +0000

    dataFinder is more concise using ternary operator

--- src/main/javascript/dataFinder.js
+++ src/main/javascript/dataFinder.js
@@ -1,16 +1,12 @@
 const dataForCategory = (title, categories) => {
     const matchingCategories = categories.filter(item => item.title === title);
-    if (matchingCategories.length === 1) {
-        return matchingCategories[0].data
-    } else if (matchingCategories.length === 0) {
-        console.log('No matching categories for title:' + title);
-        return []
-    } else if (matchingCategories.length > 1) {
+    if (matchingCategories.length > 1) {
         throw {
             name: 'Too many matching categories',
             message: `${matchingCategories.length} categories are titled '${title}'`
         };
     }
+    return matchingCategories.length === 1 ? matchingCategories[0].data : []
 };
 
 module.exports = dataForCategory;
